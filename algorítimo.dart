85db6438910920e21ad416dd9157881f48f454b7
WallisonBruno <wallyson-bruno@hotmail.com>
2021-03-05 11:49:12 -0300

    primeiro commit do arquivo

---
+++ algorítimo.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print("#####################################################");
+  print("Digite seu nome?");
+  // Reading name of the Geek
+  String name = stdin.readLineSync();
+
+  // Printing the name
+  print("Olá, $name! \!!!");
+  print("#####################################################");
+}
