1fded907706118b0e6185679d32225eab8f88606
WallisonBruno <wallyson-bruno@hotmail.com>
2021-03-06 11:36:15 -0300

    algorimo funcionando

--- algorítimo.dart
+++ algorítimo.dart
@@ -1,12 +1,39 @@
 import 'dart:io';
+import 'dart:math' as math;
 
 void main() {
-  print("#####################################################");
-  print("Digite seu nome?");
-  // Reading name of the Geek
-  String name = stdin.readLineSync();
+  print("###################### INÍCIO ############################");
 
-  // Printing the name
-  print("Olá, $name! \!!!");
-  print("#####################################################");
+  double galao;
+  int numero_de_garrafas;
+  List garrafas;
+  String saida = "";
+
+  print("Quantos litros tem o galão de água?");
+  galao = double.tryParse(stdin.readLineSync());
+
+  print("Quantas garrafas são?");
+  numero_de_garrafas = int.tryParse(stdin.readLineSync());
+  garrafas = new List(numero_de_garrafas);
+
+  int contador = 0;
+
+  for (var i in garrafas) {
+    print("Quantos litros tem a garrafa ${contador + 1}");
+    garrafas[contador] = double.tryParse(stdin.readLineSync());
+    contador++;
+  }
+
+  garrafas.sort();
+
+  for (var garrafa in garrafas.reversed) {
+    print(garrafa);
+    if (galao - garrafa >= 0) {
+      galao = galao - garrafa;
+      saida = saida + " ${garrafa}L";
+    }
+  }
+  print('Garrfas: ${saida}');
+  print('Sobra: ${galao.toStringAsFixed(1)}L');
+  print("######################### FIM ############################");
 }
